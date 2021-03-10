23e894c88ed92d9d6716424cf1c743a127326dbd
Nicole Kim <na_yeon_kim@brown.edu>
2021-03-09 21:53:52 -0800

    added

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,9 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="constrain q-pa-md">
     <div>
       <profile-header></profile-header>
     </div>
-    <h5>Profile Page</h5>
+      <div class="row">
+      <q-item> 
+
+        <q-item-section avatar class="col-6">
+          <q-avatar
+            size="90px"
+          >
+            <q-img
+              src="../images/janedoe.jpg"
+            >
+            </q-img>
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section class="col-3">
+          <q-item-label>6</q-item-label>
+          <q-item-label caption>Following</q-item-label>
+        </q-item-section>
+        <q-item-section class="col-3">
+          <q-item-label>800</q-item-label>
+          <q-item-label caption>Followers</q-item-label>
+        </q-item-section>
+        <q-item-section class="col-3">
+          <q-item-label>700</q-item-label>
+          <q-item-label caption>Following</q-item-label>
+        </q-item-section>
+      </q-item>
+      </div>
   </q-page>
 </template>
 
