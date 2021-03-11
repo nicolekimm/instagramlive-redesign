95765cf97eb4e7f66490567d0d52594fe00be663
joannehan1 <jhan13@risd.edu>
2021-03-11 16:15:16 -0500

    adding content to card

--- src/components/ScheduleTab.vue
+++ src/components/ScheduleTab.vue
@@ -1,18 +1,45 @@
 <template>
-      <q-card style="width: 100%">
+    <q-card class="my-card" style="width: 100%">
+      <q-card-section>
+      <q-card-section>
+        <center>
+        <div class="text-h6">Schedule a Live</div>
+        <!-- <div class="text-subtitle2">Subtitle</div> -->
+        </center>
+      </q-card-section>
 
-        <q-card-section class="row items-center">
+      <q-card-actions>
+        <q-btn flat round icon="event"></q-btn>
+        <q-btn flat>
+          7:30PM
+        </q-btn>
+        <q-btn flat color="primary">
+          Pick time
+        </q-btn>
+      </q-card-actions>
+
+      <q-separator></q-separator>
+       
+      <q-card-actions vertical>
+        <q-btn flat>Action 1</q-btn>
+        <q-separator></q-separator>
+       
+        <div class="text-subtitle2" style="font-weight:bold;">Pick your audience
+          <p>blah</p>
+          <p>blah</p>
+          <p>blah</p>
+          
+          
+          </div>
+
+
+        <q-btn>Schedule</q-btn>
         
-            
-            <div class="text-weight-bold">Schedule a Live</div>
-            <div class="text-grey">Title</div>
-            <div class="text-grey">Date</div>
-            <div class="text-grey">Category</div>
+      </q-card-actions>
+      </q-card-section>
+    </q-card>
 
-          
-          
-        </q-card-section>
-      </q-card>
+      
          
 
           
@@ -33,9 +60,9 @@ export default {
 
 <script>
 
-// <style lang="sass">
-//     // .q-header
-//     //   .q-toolbar__title
-//         // font-size: 25px
-//         // font-weight: bold
-// </style>
+<style lang="sass">
+    // .q-header
+    //   .q-toolbar__title
+        // font-size: 25px
+        // font-weight: bold
+</style>
