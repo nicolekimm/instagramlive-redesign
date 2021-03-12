175ef2c22dacc367a12e3e626d408536de465796
ragnaag <ragna_agerup@brown.edu>
2021-03-11 19:35:52 -0500

    Categories - sport

---
+++ src/components/CategoriesHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <q-header
+        class="bg-white text-grey-10"
+        bordered>
+        <q-toolbar>
+            <q-toolbar-title>
+            Sports
+            </q-toolbar-title>
+            <div>
+              <span class="material-icons md-48" style="font-size: 25px; padding:2px">
+                add_circle
+              </span>
+              <span class="material-icons md-48" style="font-size: 25px; padding:2px" >
+                event
+              </span>
+              <span class="material-icons md-48" style="font-size: 30px; padding:2px">
+                menu
+              </span>
+            </div>
+        </q-toolbar>
+    </q-header>
+</template>
+
+<script>
+
+export default {
+  name: 'CategoriesHeader',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+    .q-header
+      .q-toolbar__title
+        font-size: 25px
+        font-weight: bold
+</style>
