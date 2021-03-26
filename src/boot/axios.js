84784f8806865a7eb17dec3aa3469f2ca7b7fa45
ragnaag <ragna_agerup@brown.edu>
2021-03-25 22:13:33 -0400

    update for checkpoint

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,3 +2,19 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
+
+const api = axios.create({ baseURL: 'https://www.googleapis.com/youtube/v3/videos' })
+Vue.prototype.$api = api
+// ^ ^ ^ this will allow you to use this.$api
+//       so you can easily perform requests against your app's API
+
+// const fs = require('fs')
+// fs.readFile('../../categories.json', 'utf8', (err, jsonString) => {
+//     if (err) {
+//         console.log("File read failed:", err)
+//         return
+//     }
+//     console.log('File data:', jsonString) 
+// })
+
+export { axios, api }
